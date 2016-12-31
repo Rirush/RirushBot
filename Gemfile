04f69c4d9fa5b430acaefa493f571d7afeaccc25
Rirush <endernyasha11@gmail.com>
2016-12-31 16:14:39 +0800

    Download

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "faraday"
 gem 'sucker_punch', '~> 2.0'
+gem 'faraday-cookie_jar'
--- bot.rb
+++ bot.rb
@@ -2,11 +2,19 @@ require 'sinatra'
 require 'faraday'
 require 'json'
 require 'sucker_punch'
+require 'faraday-cookie_jar'
 
 fd = Faraday.new(:url => "https://api.telegram.org") do |faraday|
-  faraday.request     :url_encoded
-  faraday.response    :logger
-  faraday.adapter     Faraday.default_adapter
+  faraday.request  :url_encoded
+  faraday.response :logger
+  faraday.adapter  Faraday.default_adapter
+end
+
+osu = Faraday.new(:url => "https://osu.ppy.sh") do |faraday|
+  faraday.request  :url_encoded
+  faraday.response :logger
+  faraday.adapter  Faraday.default_adapter
+  faraday.use      :cookie_jar
 end
 
 fd.post "/bot#{ENV['TOKEN']}/setWebhook", { :url => "https://rirushbot.herokuapp.com/hook/#{ENV['SECRETADDR']}/RirushBot/" }
@@ -15,6 +23,7 @@ before do
   request.body.rewind
   @request_payload = JSON.parse request.body.read
 end
+
 post "/hook/#{ENV['SECRETADDR']}/RirushBot/" do
   puts @request_payload
   if (/^\/ping*/ =~ @request_payload['message']['text']) != nil then
@@ -25,9 +34,9 @@ post "/hook/#{ENV['SECRETADDR']}/RirushBot/" do
     }
   end
   if (/\/osu http[s]:\/\/osu.ppy.sh\/s\/(?<id>\d+)/ =~ @request_payload['message']['text']) != nil then
-    BeatmapDownload.perform_async /\/osu http[|s]:\/\/osu.ppy.sh\/s\/(?<id>\d+)/.match(@request_payload['message']['text'])[:id]
+    BeatmapDownload.perform_async /\/osu http[|s]:\/\/osu.ppy.sh\/s\/(?<id>\d+)/.match(@request_payload['message']['text'])[:id], @request_payload['message']['chat']['id'], @request_payload['message']['message_id']
     fd.post "/bot#{ENV['TOKEN']}/sendMessage", {
-        :chat_id => @request_payload['message']['from']['id'],
+        :chat_id => @request_payload['message']['chat']['id'],
         :text => "Your beatmap going to be downloaded soon",
         :reply_to_message_id => @request_payload['message']['message_id']
     }
@@ -38,7 +47,14 @@ end
 class BeatmapDownload
   include SuckerPunch::Job
 
-  def perform(data)
-    puts data
+  def perform(beatmapid, userid, messageid)
+    osu.post "/forum/ucp.php?mode=login", { username: ENV['OSULOGIN'], password: ENV['OSUPASS'], autologin: 'on', sid: '', login: 'login' }
+    beatmap = osu.get "/d/#{beatmapid}n"
+    fd.post "/bot#{ENV['TOKEN']}/sendDocument", {
+        :chat_id => userid,
+        :text => "Your beatmap was succesfully downloaded! BeatmapID = #{beatmapid}",
+        :reply_to_message_id => messageid,
+        :document => beatmap.body
+    }
   end
 end
