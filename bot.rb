474318ca29df9d0d411146d93c7b36641f751122
Rirush <endernyasha11@gmail.com>
2016-12-31 14:43:09 +0800

    fix

--- bot.rb
+++ bot.rb
@@ -10,17 +10,17 @@ end
 
 fd.get "/bot#{ENV['TOKEN']}/setWebhook", { :url => "https://rirushbot.herokuapp.com/hook/#{ENV['SECRETADDR']}/RirushBot/" }
 
-
-before do
-  request.body.rewind
-  @request_payload = JSON.parse request.body.read
-end
-
 get "/" do
   "Bot is up!"
 end
 post "/" do
   "Bot is up!"
+end
+
+
+before do
+  request.body.rewind
+  @request_payload = JSON.parse request.body.read
 end
 post "/hook/#{ENV['SECRETADDR']}/RirushBot}" do
   puts @request_payload
