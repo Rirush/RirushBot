9b1c978e152dcb48c35123613ffa9677bdec6e38
Rirush <endernyasha11@gmail.com>
2016-12-31 17:15:41 +0800

    Rescue

--- bot.rb
+++ bot.rb
@@ -27,6 +27,12 @@ end
 
 post "/hook/#{ENV['SECRETADDR']}/RirushBot/" do
   puts @request_payload
+  begin
+    test = @request_payload['message']['text']
+  rescue
+    "ok"
+  end
+
   if (/^\/ping*/ =~ @request_payload['message']['text']) != nil then
     fd.post "/bot#{ENV['TOKEN']}/sendMessage", {
         :chat_id => @request_payload['message']['from']['id'],
