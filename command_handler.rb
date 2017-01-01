4a0222ce9f535fd809a31778e3476b64d4fafd4a
Rirush <endernyasha11@gmail.com>
2017-01-02 00:53:53 +0800

    Can I fix this?

--- command_handler.rb
+++ command_handler.rb
@@ -6,7 +6,7 @@ class CommandHandler
   include SuckerPunch::Job
 
   def perform(payload)
-    command = /^\/(?<command>[\w\d]+)(?:@RirushBot)(?:\s(?<args>.*))?$/i.match(payload['text'])
+    command = /^\/(?<command>[\w\d]+)(?:@RirushBot)(?:\s(?<args>.*))?$/i.match(payload[:text])
     args = ''
     begin
       args = command[:args]
