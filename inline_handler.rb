e287330838af60a6f4d82405d6bb0dc8b993bdf0
Rirush <Rirush@cyberdude.com>
2017-01-04 14:36:25 +0800

    Mode fix

--- inline_handler.rb
+++ inline_handler.rb
@@ -1,10 +1,12 @@
 require './define'
+require './beatmap_download'
 require 'sucker_punch'
 
 class InlineHandler
   include SuckerPunch::Job
 
   def perform(querydata, querytext)
-
+    osu = /http(?:|s):\/\/osu.ppy.sh\/s\/(?<id>\d+)/i.match(querytext)
+    BeatmapDownload.perform_async(osu['id'], querydata['from']['id'], -1, true) if osu != nil
   end
 end
