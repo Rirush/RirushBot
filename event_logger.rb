442db7bcd2487b223a02775e4822a2be805d8433
Rirush <Rirush@cyberdude.com>
2017-01-04 18:20:53 +0800

    Minor bug fixes and code improvements

--- event_logger.rb
+++ event_logger.rb
@@ -7,7 +7,7 @@ class EventLogger
   def perform(payload)
     puts 'Incoming message' if payload.has_key?('message')
     puts 'Incoming inline query' if payload.has_key?('inline_query')
-    puts "Message from #{payload['message']['from']['username']} / #{payload['mainkey']['from']['id']}" if payload.has_key?('message')
+    puts "Message from #{payload['message']['from']['username']} / #{payload['message']['from']['id']}" if payload.has_key?('message')
     puts "Message: #{payload['message']['text']}" if payload.has_key?('message')
     puts "Query: #{payload['inline_query']['query']}" if payload.has_key?('inline_query')
     puts "Payload:\n#{payload}"
